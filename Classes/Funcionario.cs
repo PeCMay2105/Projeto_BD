77d7854e8da5400dae7077897d6c2b09f41f35be
João Marcelo <159437965+jm-236@users.noreply.github.com>
2025-11-30 11:05:13 -0300

    refactor: reorganização das classes em uma pasta só.

---
+++ Classes/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patrimonioDB.Classes
+{
+    public class Funcionario : Pessoa
+    {
+        public int Id_funcao { get; set; }
+        public int SetorId { get; set; }
+        public double Salario { get; set; }
+        public DateTime DataAdmissao { get; set; }
+    }
+}
---
+++ Classes/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patrimonioDB.Classes
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Setor_Id { get; set; }
+        public DateTime DataCompra {  get; set; }
+        public DateTime? DataRemocao { get; set; }
+        public int FuncionarioResponsavel_Id { get; set; }
+        public int Quantidade { get; set; }
+        public string Descricao { get; set; }
+        public double ValorUnitario { get; set; }
+        public string NomeSetor { get; set; }
+
+        public double ValorTotal => Quantidade * ValorUnitario;
+        public bool EstaAtivo => DataRemocao == null;
+    }
+}
